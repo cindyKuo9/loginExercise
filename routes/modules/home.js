977b7983dbb14fe8062cba2c3681e0c40d748f6f
cindyKuo <qaz19901014@gmail.com>
2022-09-15 22:33:40 +0800

    feat: add login function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const accountSchema = require('../../models/account')
+const accountDb = require('../../models/account')
 
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const email = req.body.inputEmail
+  const pwd = req.body.inputPassword
+
+  accountDb.findOne({ email: email })
+    .lean()
+    .then(user => {
+      if (user) {
+        if (user.password === pwd) {
+          return res.render('welcome', { firstName: user.firstName })
+        }
+      }
+      return res.render('index')
+    })
 })
 
 module.exports = router
